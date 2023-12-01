dbd14d71a3fb83965c75cb5a6e05a0e466a7be1b
kj <kkjiang27@gmail.com>
2023-12-01 16:06:34 -0500

    Create Drop Down Menu

--- gpt-web-desktop/PreferencesViewController.swift
+++ gpt-web-desktop/PreferencesViewController.swift
@@ -5,12 +5,4 @@
 //  Created by Kuang Jiang on 12/1/23.
 //
 
-import Cocoa
-
-class PreferencesViewController: NSViewController {
-    override func loadView() {
-        // Load your preferences view from a xib or programmatically
-        let preferencesView = NSView() // Replace this with your actual preferences view
-        self.view = preferencesView
-    }
-}
+import Foundation
