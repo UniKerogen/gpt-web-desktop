f6983335de0eba8acb92dcc9c5356ce8095b5b1a
kj <kkjiang27@gmail.com>
2023-12-02 05:54:46 -0500

    New Chat Model Switcher

--- gpt-web-desktop/PreferencesViewController.swift
+++ gpt-web-desktop/PreferencesViewController.swift
@@ -7,6 +7,15 @@
 
 import Cocoa
 
+struct ChatBot{
+    var name: String
+    var data: String
+}
+
+var chatGPT = ChatBot(name:"ChatGPT", data:"https://www.chatgpt.com/")
+var wyyx = ChatBot(name:"文言一心", data:"https://yiyan.baidu.com")
+var google = ChatBot(name:"Google", data:"https://www.google.com")
+
 var targetWebsite: String?
 
 class PreferencesViewController: NSViewController {
@@ -14,11 +23,9 @@ class PreferencesViewController: NSViewController {
     @IBOutlet weak var websiteComboBox: NSComboBox!
     
     var preferencesWindowController: NSWindowController?
-    
-    // @IBOutlet var myViewController: MyViewController?
 
-    // Add your website options here
-    let websiteOptions = ["https://www.chatgpt.com/", "https://yiyan.baidu.com", "https://www.google.com"]
+    // Add website options
+    let websiteOptions = ["ChatGPT", "文言一心", "Google"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +41,24 @@ class PreferencesViewController: NSViewController {
     }
 
     @IBAction func savePreferences(_ sender: Any) {
+        // Convert Name to URL
+        let targetURL: String?
+        switch websiteComboBox.stringValue{
+        case chatGPT.name:
+            targetURL = chatGPT.data
+        case wyyx.name:
+            targetURL = wyyx.data
+        case google.name:
+            targetURL = google.data
+        default:
+            targetURL = chatGPT.data
+        }
+        
         // Save the selected website to UserDefaults
-        UserDefaults.standard.set(websiteComboBox.stringValue, forKey: "SelectedWebsite")
+        UserDefaults.standard.set(targetURL, forKey: "SelectedWebsite")
         
         // Update the selected website in the existing window controller
-        // myViewController?.selectedWebsite = websiteComboBox.stringValue
-        targetWebsite = websiteComboBox.stringValue
+        targetWebsite = targetURL
     }
     
     @IBAction func showPreferencesWindow(_ sender: Any) {
@@ -51,3 +70,4 @@ class PreferencesViewController: NSViewController {
         preferencesWindowController?.showWindow(sender)
     }
 }
+
